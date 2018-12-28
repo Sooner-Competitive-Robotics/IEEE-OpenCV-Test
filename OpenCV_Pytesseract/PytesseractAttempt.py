4a97940c076534b797fa1f7ebd18c6ca6bd8bb5f
PouyaT <pouya.g.tavakoli-1@ou.edu>
2018-12-27 21:02:12 -0600

    changed the program so it will detect the letter 'E' in the base form

--- OpenCV_Pytesseract/PytesseractAttempt.py
+++ OpenCV_Pytesseract/PytesseractAttempt.py
@@ -1,15 +1,16 @@
 import pytesseract
-from PIL import Image
+import cv2
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
 
 
 def main():
-
-    im = Image.open('E_Testing.png')
-    print(im)
-    text = pytesseract.image_to_string(im, config='--psm 10')
+    original = cv2.imread('E_Testing.png', 0)
+    # binary thresh it at value 100. It is now a black and white image
+    ret, original = cv2.threshold(original, 100, 255, cv2.THRESH_BINARY)
+    text = pytesseract.image_to_string(original, config='--psm 10')
     print(text)
+
 
 
 if __name__ == "__main__":
