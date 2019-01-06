1cea5e00ba3c642622e54e60c62287c4112c875a
PouyaT <pouya.g.tavakoli-1@ou.edu>
2019-01-05 18:45:05 -0600

    Almost works perfectly. Does not spit out the desired output for A_270 works for all other letters

--- OpenCV_Pytesseract/PytesseractAttempt.py
+++ OpenCV_Pytesseract/PytesseractAttempt.py
@@ -5,12 +5,34 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\t
 
 
 def main():
-    original = cv2.imread('E_Testing.png', 0)
+    # list of letters we are looking for
+    letters = ['A', 'B', 'C', 'D', 'E', 'F']
+    original = cv2.imread('A_Testing_270.png', 0)
     # binary thresh it at value 100. It is now a black and white image
-    ret, original = cv2.threshold(original, 100, 255, cv2.THRESH_BINARY)
+    # ret, original = cv2.threshold(original, 100, 255, cv2.THRESH_BINARY)
     text = pytesseract.image_to_string(original, config='--psm 10')
+    while text not in letters:
+        # if it does not recognize the letter it will rotate the image
+        original = rotate(original)
+        text = pytesseract.image_to_string(original, config='--psm 10')
+
     print(text)
 
+
+def rotate(image):
+    # get the image height, width
+    (h, w) = image.shape[:2]
+
+    # calculate the center of the image
+    center = (w/2, h/2)
+
+    scale = 1.0
+
+    # perform the counter clockwise rotation holding at the center
+    m = cv2.getRotationMatrix2D(center, 90, scale)
+    rotated90 = cv2.warpAffine(image, m, (w, h))
+
+    return rotated90
 
 
 if __name__ == "__main__":
