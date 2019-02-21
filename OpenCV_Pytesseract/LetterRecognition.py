8aef5d8e9b5e135684c7135a074c0265f0742dcd
Kichlids <32001401+Kichlids@users.noreply.github.com>
2019-02-21 12:26:11 -0600

    88% success rate using mix of v4 and lang

--- OpenCV_Pytesseract/LetterRecognition.py
+++ OpenCV_Pytesseract/LetterRecognition.py
@@ -1,41 +1,62 @@
-import cv2
-import pytesseract
 
+#from tesserocr import PyTessBaseAPI, RIL, iterate_level
+import pytesseract
+import cv2
+
+from tesserocr import PyTessBaseAPI
+
+# using version 4
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
 
 
 def main():
 
+    success = 0
     # list of pictures to compare
-    pictures = ['A_Testing.png', 'A_Testing_90.png', 'A_Testing_180.png', 'A_Testing_270.png',
+    picturesAll = ['A_Testing.png', 'A_Testing_90.png', 'A_Testing_180.png', 'A_Testing_270.png',
                 'B_Testing.png', 'B_Testing_90.png', 'B_Testing_180.png', 'B_Testing_270.png',
                 'C_Testing.png', 'C_Testing_90.png', 'C_Testing_180.png', 'C_Testing_270.png',
                 'D_Testing.png', 'D_Testing_90.png', 'D_Testing_180.png', 'D_Testing_270.png',
                 'E_Testing.png', 'E_Testing_90.png', 'E_Testing_180.png', 'E_Testing_270.png',
                 'F_Testing.png', 'F_Testing_90.png', 'F_Testing_180.png', 'F_Testing_270.png']
 
+    pictures0 = ['A_Testing.png', 'B_Testing.png', 'C_Testing.png','D_Testing.png','E_Testing.png','F_Testing.png']
+    pictures90 = ['A_Testing_90.png', 'B_Testing_90.png', 'C_Testing_90.png','D_Testing_90.png','E_Testing_90.png','F_Testing_90.png']
+    pictures180 = ['A_Testing_180.png','B_Testing_180.png', 'C_Testing_180.png', 'D_Testing_180.png', 'E_Testing_180.png', 'F_Testing_180.png']
+    pictures270 = ['A_Testing_270.png','B_Testing_270.png','C_Testing_270.png','D_Testing_270.png','E_Testing_270.png','F_Testing_270.png']
+    picturesA = ['A_Testing.png', 'A_Testing_90.png', 'A_Testing_180.png', 'A_Testing_270.png']
+    picturesB = ['B_Testing.png', 'B_Testing_90.png', 'B_Testing_180.png', 'B_Testing_270.png']
+    picturesC = ['C_Testing.png', 'C_Testing_90.png', 'C_Testing_180.png', 'C_Testing_270.png']
+    picturesD = ['D_Testing.png', 'D_Testing_90.png', 'D_Testing_180.png', 'D_Testing_270.png']
+    picturesE = ['E_Testing.png', 'E_Testing_90.png', 'E_Testing_180.png', 'E_Testing_270.png']
+    picturesF = ['F_Testing.png', 'F_Testing_90.png', 'F_Testing_180.png', 'F_Testing_270.png']
+
+    pictures = picturesAll
+
     # Text detect all the letters
     index = 0
-    while index < 24:
-        if (index <4):
-            print("Testing A")
-        elif (index <8):
-            print("Testing B")
-        elif (index <12):
-            print("Testing C")
-        elif (index <16):
-            print("Testing D")
-        elif (index <20):
-            print("Testing E")
-        elif (index <24):
-            print("Testing F")
+    
+    while index <  len(pictures):
+
+        textRead = ""
         name = pictures[index]
-        print(findImage(name, str(index)))
+
+        letter = name[0:1]
+        print("Testing " + letter)
+
+        textRead = findImage(name, str(index))
+        print(textRead)
+
+        if (textRead == letter):
+            success = success + 1
         index = index + 1
 
     print("Done")
+    print("Number of successes: " + str(success))
+    print("Success rate: " + str(success / len(pictures)))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
 
 def findImage(name, windowname):
     # list of letters we are looking for
@@ -43,21 +64,30 @@ def findImage(name, windowname):
     
     # read image
     image = cv2.imread(name)
+
     # convert image to grayscale
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray_image = crop(gray_image)
-    ret,thresh = cv2.threshold(gray_image, 70, 255, cv2.THRESH_BINARY)
+    gray_image = cv2.medianBlur(gray_image, 5)
+    ret,thresh = cv2.threshold(gray_image, 103, 255, cv2.THRESH_BINARY)
     cv2.imshow(windowname, thresh)
 
     # read the picture using Tesseract
-    text = pytesseract.image_to_string(thresh, lang = "Stencil",  config='-c tessedit_char_whitelist=ABCDEF --psm 10')
+    text = pytesseract.image_to_string(thresh, config='--psm 10')
+    #return text
 
     # rotates image until it recognizes it as a letter. Max rotation of 4
     counter = 0
     while text not in letters and counter < 4:
         thresh = rotate(thresh)
-        text = pytesseract.image_to_string(thresh, lang = "Stencil", config='-c tessedit_char_whitelist=ABCDEF --psm 10')
+        text = pytesseract.image_to_string(thresh, config='--psm 10')
+        
+        #text = pytesseract.image_to_string(thresh, lang = "Stencil", config='-c tessedit_char_whitelist=ABCDEF --psm 10')
         counter = counter + 1
+        print("rotated")
+
+    if text not in letters:
+        text = pytesseract.image_to_string(thresh, lang = "Stencil", config = '-c tessedit_char_whitelist=ABCDEF --psm 10')
 
     return text
 
