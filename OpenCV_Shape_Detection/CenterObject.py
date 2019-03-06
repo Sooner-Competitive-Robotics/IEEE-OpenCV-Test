cacafcc013bc0e8e840de230ca449ef7360c41e7
Kichlids <32001401+Kichlids@users.noreply.github.com>
2019-03-05 19:18:26 -0600

    Added pictures on the game field and added some functions in main

--- OpenCV_Shape_Detection/CenterObject.py
+++ OpenCV_Shape_Detection/CenterObject.py
@@ -1,42 +1,81 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("D1.jpg")  #load an image of a single battery
-img_gs = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  #convert to grayscale
 
-#inverted binary threshold: 1 for the battery, 0 for the background
-_, thresh = cv2.threshold(img_gs, 250, 1, cv2.THRESH_BINARY_INV)
+#	Returns -1 if object is to the left
+#			0 if object is in the center
+#			1 if object is to the right			
 
-#From a matrix of pixels to a matrix of coordinates of non-black points.
-#(note: mind the col/row order, pixels are accessed as [row, col]
-#but when we draw, it's (x, y), so have to swap here or there)
-mat = np.argwhere(thresh != 0)
+def main():
+	center()
 
-#let's swap here... (e. g. [[row, col], ...] to [[col, row], ...])
-mat[:, [0, 1]] = mat[:, [1, 0]]
-#or we could've swapped at the end, when drawing
-#(e. g. center[0], center[1] = center[1], center[0], same for endpoint1 and endpoint2),
-#probably better performance-wise
+def center():
+	img = cv2.imread("Center.jpg")  #load an image of a single battery
+	img_gs = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  #convert to grayscale
+	
+	#inverted binary threshold: 1 for the battery, 0 for the background
+	_, thresh = cv2.threshold(img_gs, 200, 250, cv2.THRESH_BINARY_INV)
+	
+	#From a matrix of pixels to a matrix of coordinates of non-black points.
+	#(note: mind the col/row order, pixels are accessed as [row, col]
+	#but when we draw, it's (x, y), so have to swap here or there)
+	mat = np.argwhere(thresh != 0)
+	
+	#let's swap here... (e. g. [[row, col], ...] to [[col, row], ...])
+	mat[:, [0, 1]] = mat[:, [1, 0]]
+	#or we could've swapped at the end, when drawing
+	#(e. g. center[0], center[1] = center[1], center[0], same for endpoint1 and endpoint2),
+	#probably better performance-wise
+	
+	mat = np.array(mat).astype(np.float32) #have to convert type for PCA
+	
+	#mean (e. g. the geometrical center) 
+	#and eigenvectors (e. g. directions of principal components)
+	m, e = cv2.PCACompute(mat, mean = np.array([]))
+	
+	#now to draw: let's scale our primary axis by 100, 
+	#and the secondary by 50
+	
+	center = tuple(m[0])					# THE CENTER POINT!
+	endpoint1 = tuple(m[0] + e[0]*100)
+	endpoint2 = tuple(m[0] + e[1]*50)
+	
+	red_color = (0, 0, 255)
+	cv2.circle(img, center, 5, red_color)
+	cv2.line(img, center, endpoint1, red_color)
+	cv2.line(img, center, endpoint2, red_color)
+	cv2.imshow("Threshold", thresh)
+	cv2.imshow("Img", img)
+	
+	imageSize = img.shape[:2]
+	imageCenter = img.shape[:2]
+	imageCenter = (imageCenter[0]/2, imageCenter[1]/2)
+	
+	CENTER_RANGE = 5
+	
+	cv2.waitKey(0)
+	
+	#cv2.circle(img, int(imageCenter), 5, (0,0,0))
+	
+	print("Center of obj: " + str(center))
+	print("Size of the image: " + str(imageSize))
+	print("The center coordinate: " + str((imageSize[0]/2, imageSize[1]/2)))
+	print("The difference: " + str(center[1] - imageCenter[1]))
+	
+	cv2.line(img, (int(imageCenter[0]), int(imageCenter[1])), (int(imageSize[0]/2), int(imageSize[1])), (0,0,255))
+	
+	if center[1] - imageCenter[1] > CENTER_RANGE:
+		print("Object is to the left")
+		return 1
+	elif center [1] - imageCenter[1] < -CENTER_RANGE:
+		print("Object is to the right")
+		return -1
+	else:
+		print("Object is in the center")
+		return 0
+	
 
 
-mat = np.array(mat).astype(np.float32) #have to convert type for PCA
-
-#mean (e. g. the geometrical center) 
-#and eigenvectors (e. g. directions of principal components)
-m, e = cv2.PCACompute(mat, mean = np.array([]))
-
-#now to draw: let's scale our primary axis by 100, 
-#and the secondary by 50
-
-center = tuple(m[0])					# THE CENTER POINT!
-endpoint1 = tuple(m[0] + e[0]*100)
-endpoint2 = tuple(m[0] + e[1]*50)
-
-red_color = (0, 0, 255)
-cv2.circle(img, center, 5, red_color)
-cv2.line(img, center, endpoint1, red_color)
-cv2.line(img, center, endpoint2, red_color)
-cv2.imshow("out.png", img)
-cv2.waitKey(0)
  
-
+if __name__ == "__main__":
+    main()
--- OpenCV_Shape_Detection/ShapeDetection.py
+++ OpenCV_Shape_Detection/ShapeDetection.py
@@ -1,70 +1,77 @@
 import cv2
 import numpy as np
 
-# Read image
-img = cv2.imread("OneCube.jpg")
-cv2.namedWindow("Original Image",cv2.WINDOW_NORMAL)
-cv2.imshow("Original Image",img)
+def main():
+	findShape()
 
-# RGB to Gray scale conversion
-img_gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-cv2.namedWindow("Gray Converted Image",cv2.WINDOW_NORMAL)
-cv2.imshow("Gray Converted Image",img_gray)
 
-# Noise removal with iterative bilateral filter(removes noise while preserving edges)
-noise_removal = cv2.bilateralFilter(img_gray,9,75,75)
-cv2.namedWindow("Noise Removed Image",cv2.WINDOW_NORMAL)
-cv2.imshow("Noise Removed Image",noise_removal)
+def findShape():
+	# Read image
+	img = cv2.imread("OneCube.jpg")
+	cv2.namedWindow("Original Image",cv2.WINDOW_NORMAL)
+	cv2.imshow("Original Image",img)
+	
+	# RGB to Gray scale conversion
+	img_gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+	cv2.namedWindow("Gray Converted Image",cv2.WINDOW_NORMAL)
+	cv2.imshow("Gray Converted Image",img_gray)
+	
+	# Noise removal with iterative bilateral filter(removes noise while preserving edges)
+	noise_removal = cv2.bilateralFilter(img_gray,9,75,75)
+	cv2.namedWindow("Noise Removed Image",cv2.WINDOW_NORMAL)
+	cv2.imshow("Noise Removed Image",noise_removal)
+	
+	# Thresholding the image
+	ret,thresh_image = cv2.threshold(noise_removal,0,255,cv2.THRESH_OTSU)
+	cv2.namedWindow("Image after Thresholding",cv2.WINDOW_NORMAL)
+	cv2.imshow("Image after Thresholding",thresh_image)
+	
+	# Applying Canny Edge detection
+	canny_image = cv2.Canny(thresh_image,250,255)
+	cv2.namedWindow("Image after applying Canny",cv2.WINDOW_NORMAL)
+	cv2.imshow("Image after applying Canny",canny_image)
+	canny_image = cv2.convertScaleAbs(canny_image)
+	
+	# dilation to strengthen the edges
+	kernel = np.ones((3,3), np.uint8)
+	# Creating the kernel for dilation
+	dilated_image = cv2.dilate(canny_image,kernel,iterations=1)
+	cv2.namedWindow("Dilation", cv2.WINDOW_NORMAL)
+	cv2.imshow("Dilation", dilated_image)
+	contours, h = cv2.findContours(dilated_image, 1, 2)
+	contours = sorted(contours, key = cv2.contourArea, reverse = True)[:1]
+	pt = (180, 3 * img.shape[0] // 4)
+	
+	for cnt in contours:
+		approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
+	
+		if len(approx) == 6:
+			print("Cube")
+			cv2.drawContours(img,[cnt],-1,(255,0,0),3)
+			cv2.putText(img,'Cube', pt ,cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, [0, 255, 255], 2)
+		elif len(approx) == 7:
+			print("Cube")
+			cv2.drawContours(img,[cnt],-1,(255,0,0),3)
+			cv2.putText(img,'Cube', pt ,cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, [0, 255, 255], 2)
+		elif len(approx) == 8:
+			print("Cylinder")
+			cv2.drawContours(img,[cnt],-1,(255,0,0),3)
+			cv2.putText(img,'Cylinder', pt ,cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, [0, 255, 255], 2)
+		elif len(approx) > 10:
+			print("Sphere")
+			cv2.drawContours(img,[cnt],-1,(255,0,0),3)
+			cv2.putText(img,'Sphere', pt ,cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, [255, 0, 0], 2)
+	
+	cv2.namedWindow("Shape", cv2.WINDOW_NORMAL)
+	cv2.imshow('Shape',img)
+	corners = cv2.goodFeaturesToTrack(thresh_image,6,0.06,25)
+	corners = np.float32(corners)
+	for item in corners:
+		x,y = item[0]
+		cv2.circle(img,(x,y),10,255,-1)
+	cv2.namedWindow("Corners", cv2.WINDOW_NORMAL)
+	cv2.imshow("Corners",img)
+	cv2.waitKey()
 
-# Thresholding the image
-ret,thresh_image = cv2.threshold(noise_removal,0,255,cv2.THRESH_OTSU)
-cv2.namedWindow("Image after Thresholding",cv2.WINDOW_NORMAL)
-cv2.imshow("Image after Thresholding",thresh_image)
-
-# Applying Canny Edge detection
-canny_image = cv2.Canny(thresh_image,250,255)
-cv2.namedWindow("Image after applying Canny",cv2.WINDOW_NORMAL)
-cv2.imshow("Image after applying Canny",canny_image)
-canny_image = cv2.convertScaleAbs(canny_image)
-
-# dilation to strengthen the edges
-kernel = np.ones((3,3), np.uint8)
-# Creating the kernel for dilation
-dilated_image = cv2.dilate(canny_image,kernel,iterations=1)
-cv2.namedWindow("Dilation", cv2.WINDOW_NORMAL)
-cv2.imshow("Dilation", dilated_image)
-contours, h = cv2.findContours(dilated_image, 1, 2)
-contours = sorted(contours, key = cv2.contourArea, reverse = True)[:1]
-pt = (180, 3 * img.shape[0] // 4)
-
-for cnt in contours:
-	approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
-
-	if len(approx) == 6:
-		print("Cube")
-		cv2.drawContours(img,[cnt],-1,(255,0,0),3)
-		cv2.putText(img,'Cube', pt ,cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, [0, 255, 255], 2)
-	elif len(approx) == 7:
-		print("Cube")
-		cv2.drawContours(img,[cnt],-1,(255,0,0),3)
-		cv2.putText(img,'Cube', pt ,cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, [0, 255, 255], 2)
-	elif len(approx) == 8:
-		print("Cylinder")
-		cv2.drawContours(img,[cnt],-1,(255,0,0),3)
-		cv2.putText(img,'Cylinder', pt ,cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, [0, 255, 255], 2)
-	elif len(approx) > 10:
-		print("Sphere")
-		cv2.drawContours(img,[cnt],-1,(255,0,0),3)
-		cv2.putText(img,'Sphere', pt ,cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, [255, 0, 0], 2)
-
-cv2.namedWindow("Shape", cv2.WINDOW_NORMAL)
-cv2.imshow('Shape',img)
-corners = cv2.goodFeaturesToTrack(thresh_image,6,0.06,25)
-corners = np.float32(corners)
-for item in corners:
-	x,y = item[0]
-	cv2.circle(img,(x,y),10,255,-1)
-cv2.namedWindow("Corners", cv2.WINDOW_NORMAL)
-cv2.imshow("Corners",img)
-cv2.waitKey()
-
+if __name__ == "__main__":
+    main()
